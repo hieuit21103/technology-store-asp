6ea2416a930a194e2fac467d319b2dfb6559927f
Lê Minh Hiếu <hieuit21103@gmail.com>
2025-06-10 15:41:05 +0700

    create Home.jsx

--- Program.cs
+++ Program.cs
@@ -3,8 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
